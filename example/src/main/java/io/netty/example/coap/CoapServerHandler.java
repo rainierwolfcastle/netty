adaa0568b54f1ad6f85a8dbebefa6da829c31672
rainierwolfcastle <das.bobby2000@gmail.com>
2014-09-18 16:30:59 +0100

    initial commit

---
+++ example/src/main/java/io/netty/example/coap/CoapServerHandler.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.example.coap;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+
+public class CoapServerHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, DatagramPacket packet) throws Exception {
+        System.err.println(packet);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        // We don't close the channel because we can keep serving requests.
+    }
+}
